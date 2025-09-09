4a882b8581df70f3f61d2df48b582e382af8681e
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2025-09-09 15:55:12 -0500

    2.10 Nav Menu complete. 2.11 Books Routing begin.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,13 @@ import LibraryLogo from '../assets/Library.svg'
 
 
 const Nav = () => {
+    function openMenu() {
+        document.body.classList += " menu--open";
+    }
+    function closeMenu() {
+        document.body.classList.remove("menu--open");
+    }
+
     return (
         <nav>
             <div className="nav__container">
@@ -21,7 +28,7 @@ const Nav = () => {
                         Books
                         </a>
                     </li>
-                    <button className="btn__menu">
+                    <button className="btn__menu" onClick={openMenu}>
                         <FontAwesomeIcon icon="bars" />
                     </button>
                     <li className="nav__icon">
@@ -31,11 +38,11 @@ const Nav = () => {
                         <span className="cart__length">2</span>
                     </li>
                 </ul>
-                <div className="manu__backdrop">
-                    <button className="btn__menu btn__menu--close">
+                <div className="menu__backdrop">
+                    <button className="btn__menu btn__menu--close" onClick={closeMenu}>
                         <FontAwesomeIcon icon="times" />
                     </button>
-                    <ul className="manu__links">
+                    <ul className="menu__links">
                         <li className="menu__list">
                             <a href="/" className="menu__link">Home</a>
                         </li>
