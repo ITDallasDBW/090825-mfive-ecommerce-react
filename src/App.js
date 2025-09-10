38df4c601ecb65975fa29339d540b20333fd0645
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2025-09-09 20:03:44 -0500

    2.11 Books Routing complete. 2.12 Filtering begin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
-import Featured from './components/Featured';
 import Footer from './components/Footer';
-import Highlights from './components/Highlights';
-import Landing from './components/Landing';
 import Nav from './components/Nav';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Books from './pages/Books';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav /> 
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" exact component={Books} />
       <Footer />
-
     </div>
+    </Router>
   );
 }
 
