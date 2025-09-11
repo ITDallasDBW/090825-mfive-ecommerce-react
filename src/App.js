889a8300e4dc96f1999454b4687aaa9a684009c2
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2025-09-10 22:35:02 -0500

    2.13 Dynamic Routes complete. 2.14 Recommended Books begin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import Footer from './components/Footer';
-import Nav from './components/Nav';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './pages/Home';
-import Books from './pages/Books';
-import { books } from './data';
-import BookInfo from './pages/BookInfo';
+import "./App.css";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Nav /> 
-      <Route path="/" exact component={Home} />
-      <Route path="/books" render={() => <Books books={books} />} />
-      <Route path="/books/1" render={() => <BookInfo books={books} />} />
-      <Footer />
-    </div>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faBolt, faBookOpen, faShoppingCart, faStar, faStarHalfAlt, faTags, faTimes} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faBolt,
+  faBookOpen,
+  faShoppingCart,
+  faStar,
+  faStarHalfAlt,
+  faTags,
+  faTimes,
+  faArrowLeft
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+library.add(
+  faBars,
+  faShoppingCart,
+  faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+  faStar,
+  faStarHalfAlt,
+  faArrowLeft
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
