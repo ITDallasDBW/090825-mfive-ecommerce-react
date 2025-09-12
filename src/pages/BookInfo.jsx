81b6e456a4962f827dc075a1f0661fa836bacea0
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2025-09-12 09:19:51 -0500

    Revert "2.14 Recommended Books complete. 2.15 Add To Cart begin"
    This reverts commit 9f25e05bda40ddebf07a08d578e10280b1ad9f25.

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,23 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import Rating from "../components/ui/Rating";
 import Price from "../components/ui/Price";
-import Book from "../components/ui/Book";
 
-const BookInfo = ({ books, addToCart, cart }) => {
-    const {id} = useParams();
-    const book = books.find(book => +book.id === +id);
-
-    function addBookToCart(book) {
-        addToCart(book);
-    }
-
-    function bookExistsOnCart() {
-      return cart.find(book => +book.id === +id);
-    }
-
-    return (
+const BookInfo = ({ books }) => {
+  return (
     <div id="books__body">
       <main id="books__main">
         <div className="books__container">
@@ -33,54 +21,18 @@ const BookInfo = ({ books, addToCart, cart }) => {
             <div className="book__selected">
               <figure className="book__selected--figure">
                 <img
-                  src={book.url}
+                  src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg"
+                  alt=""
                   className="book__selected--img"
                 />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">
-                  {book.title}
-                </h2>
-                <Rating rating={book.rating} />
+                <h2 className="book__selected--title">Crack the Coding Interview</h2>
+                <Rating rating="4.5" />
                 <div className="book__selected--price">
-                  <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+                    <Price originalPrice={50} salePrice={20} />
                 </div>
-                <div className="book__summary">
-                  <h3 className="book__summary--title">Summary</h3>
-                  <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Labore, accusamus modi? Nulla hic velit, soluta magni
-                    facilis libero ad quidem error. Perspiciatis, praesentium
-                    labore consequuntur quaerat quo itaque unde ratione?
-                  </p>
-                  <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Labore, accusamus modi? Nulla hic velit, soluta magni
-                    facilis libero ad quidem error. Perspiciatis, praesentium
-                    labore consequuntur quaerat quo itaque unde ratione?
-                  </p>
-                </div>
-                { bookExistsOnCart() ? (
-                  console.log("Added to cart")
-                  // <button className="btn">Checkout</button>
-                ) : (
-                <button className="btn" onClick={() =>addBookToCart(book)}>Add to Cart</button>
-                )
-                }
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="books__container">
-          <div className="r">
-            <div className="book__selected--top">
-              <h2 className="book__selected--title--top">Recommended Books</h2>
-            </div>
-            <div className="books">
-
-            {books.filter((book) => book.rating === 5 && +book.id !== +id)
-            .slice(0, 4)
-            .map(book => <Book book={book} key={book.id} />)}
             </div>
           </div>
         </div>
